867e5c28add369591b90015e8eaa01d1bc52d64e
LysandreJik <lysandre.debut@reseau.eseo.fr>
2019-06-22 19:31:37 -0400

    System class-based architecture

--- System.ts
+++ System.ts
@@ -1,41 +1,57 @@
-import { Page, launch } from "puppeteer";
+import { Page, launch, Browser } from "puppeteer";
 
-(async () => {
-	const browser = await launch({ headless: false });
-	const [page] = await browser.pages();
-	page.goto("https://www.dofus.com/fr/creer-un-compte");
-
-	await page.waitForSelector("#userlogin");
-	await page.type("#userlogin", "Username1345542");
-	await page.waitForSelector("#user_password");
-	await page.type("#user_password", "password123NiceEh");
-	await page.waitForSelector("#user_mail");
-	await page.type("#user_mail", "lysandre@hotmail.com");
-	await page.select("#ak_field_1", "1");
-	await page.select("#ak_field_2", "1");
-	await page.select("#ak_field_3", "1996");
-
-	// To remove focus from the mail field so that the error event can be triggered.
-	await page.click("#userlogin");
-	await page.waitFor(500);
-	let errorFields = await checkFields(page);
-	if (errorFields.length) {
-		throw new Error("Field error " + JSON.stringify(errorFields));
-		process.exit();
+class System{
+	browser?: Browser;
+	page?: Page;
+	
+	constructor(){
+		this.launchBrowser()
 	}
+	
+	launchBrowser = async () => {
+		this.browser = await launch({ headless: false });
+		[this.page] = await this.browser.pages()
+	}
+	
+	createAccount = async (username?: string, password?: string, date?: {day: number, month: number, year: number}) => {
+		if(!this.page || !this.browser){
+			return
+		}
+		
+		await this.page.goto("https://www.dofus.com/fr/creer-un-compte");
+		await this.page.waitForSelector("#userlogin");
+		await this.page.type("#userlogin", "Username1345542");
+		await this.page.waitForSelector("#user_password");
+		await this.page.type("#user_password", "password123NiceEh");
+		await this.page.waitForSelector("#user_mail");
+		await this.page.type("#user_mail", "lolkay@blackfalcon.fr");
+		await this.page.select("#ak_field_1", "1");
+		await this.page.select("#ak_field_2", "1");
+		await this.page.select("#ak_field_3", "1996");
 
-	await page.click("#ak_field_4");
-})();
+		// To remove focus from the mail field so that the error event can be triggered.
+		await this.page.click("#userlogin");
+		await this.page.waitFor(500);
+		let errorFields = await this.checkFields(this.page);
+		if (errorFields.length) {
+			throw new Error("Field error " + JSON.stringify(errorFields));
+			process.exit();
+		}
 
-async function checkFields(page: Page) {
-	const elements = await page.$$eval(".error", elements => {
-		console.log(elements);
-		return elements
-			.map(element => {
-				return { type: (element as HTMLLabelElement).htmlFor, value: element.innerHTML };
-			})
-			.filter(element => element.value !== "");
-	});
-
-	return elements;
+		await this.page.click("#ak_field_4");
+	}
+	
+	async checkFields(page: Page) {
+		const elements = await page.$$eval(".error", elements => {
+			console.log(elements);
+			return elements
+				.map(element => {
+					return { type: (element as HTMLLabelElement).htmlFor, value: element.innerHTML };
+				})
+				.filter(element => element.value !== "");
+		});
+	
+		return elements;
+	}
 }
+
